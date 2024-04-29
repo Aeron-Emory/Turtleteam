3f9563f1508a6ac81d7e2f64205c4d6b1fe4bd89
Aeron-Emory <57010069+Aeron-Emory@users.noreply.github.com>
2024-04-29 19:03:35 +0000

    funni

--- main.py
+++ main.py
@@ -29,16 +29,24 @@ def main():
   turtle.done()
   # draw_text(screen, text_letter)
 
+
 # function 1 (background color) # Coded by Silver, Looped by Maylad Hanna
 def set_background_color(screen):
   color = str
   while True:
-    color = input("Enter the desired background color (Red, Blue, Green, etc.): ")
-    if color.lower() in ["red", "blue", "green", "yellow", "orange", "pink", "brown", "cyan", "black", "white", "purple", "grey"]:  # Add more colors if needed
-        break
+    color = input(
+        "Enter the desired background color (Red, Blue, Green, etc.): ")
+    if color.lower() in [
+        "red", "blue", "green", "yellow", "orange", "pink", "brown", "cyan",
+        "black", "white", "purple", "grey"
+    ]:  # Add more colors if needed
+      break
     else:
-        print("Invalid color. Please choose from: Red, Blue, Green, Yellow, Orange, Pink, Purple, Brown, Grey, Black, White")
+      print(
+          "Invalid color. Please choose from: Red, Blue, Green, Yellow, Orange, Pink, Purple, Brown, Grey, Black, White"
+      )
   screen.bgcolor(color)
+
 
 # function 2 (logo back design / pattern options?) # Foundation coded by Sm Fardeen Haque, modified by Silver Dankha, and looped by Maylad Hanna
 def draw_grid_pattern(screen):
@@ -47,59 +55,68 @@ def draw_grid_pattern(screen):
   color = str()
   # Main Code for the function
   while True:
-      color = input("Enter the desired color for the pattern (Red, Blue, Green, etc.): ")
-      if color.lower() in ["red", "blue", "green", "yellow", "orange", "pink", "brown", "cyan", "black", "white", "purple", "grey"]:
-          break
-      else:
-          print("Invalid color. Please choose from: Red, Blue, Green, Yellow, Orange, Pink, Purple, Brown, Grey, Black, White")
+    color = input(
+        "Enter the desired color for the pattern (Red, Blue, Green, etc.): ")
+    if color.lower() in [
+        "red", "blue", "green", "yellow", "orange", "pink", "brown", "cyan",
+        "black", "white", "purple", "grey"
+    ]:
+      break
+    else:
+      print(
+          "Invalid color. Please choose from: Red, Blue, Green, Yellow, Orange, Pink, Purple, Brown, Grey, Black, White"
+      )
   # change color
   turtle.pencolor(color)
 
-    #ask the user for what pattern they would like and what color they want it to be
+  #ask the user for what pattern they would like and what color they want it to be
   while True:
-      print("\nSelect desired pattern shape")
-      print("1. Grid\n2. Stripes\n3. Diagonal\n4. Dot\n5. Diagrid")
-      pattern = input("Enter a pattern: ")
-      width, height = screen.window_width(), screen.window_height()
-      if pattern == "Grid":
-          Verticle(width, height)
-          Horizontal(width, height)
-          break
-        #stripes code
-      elif pattern == "Stripes":
-          stripe_direction = input("Enter stripe direction (horizontal/vertical): ")
-          if stripe_direction == "horizontal":
-              Horizontal(width, height)
-          elif stripe_direction == "vertical":
-              Verticle(width, height)
-          else:
-              print("Invalid stripe direction. Please enter 'horizontal' or 'vertical'.")
-          break
-      # diagonal code
-      elif pattern == "Diagonal":
-          dir = input("Enter direction (left/right): ")
-          if dir == "left":
-              Diagonal(width, height)
-              break
-          elif dir == "right":
-              DiagonalR(width, height)
-              break
-          else:
-              print("Invalid direction. Please enter 'left' or 'right'.")
-      # dot code
-      elif pattern == "Dot":
-          dot_size = int(input("Size of each Dots: "))
-          spacing = int(input("Spacing between each Dots: "))
-          DotPattern(dot_size, spacing, width, height)
-          break
-      # diagrid code
-      elif pattern == "Diagrid":
-          Diagonal(width, height)
-          DiagonalR(width, height)
-          break
+    print("\nSelect desired pattern shape")
+    print("1. Grid\n2. Stripes\n3. Diagonal\n4. Dot\n5. Diagrid")
+    pattern = input("Enter a pattern: ")
+    width, height = screen.window_width(), screen.window_height()
+    if pattern == "Grid":
+      Verticle(width, height)
+      Horizontal(width, height)
+      break
+    #stripes code
+    elif pattern == "Stripes":
+      stripe_direction = input(
+          "Enter stripe direction (horizontal/vertical): ")
+      if stripe_direction == "horizontal":
+        Horizontal(width, height)
+      elif stripe_direction == "vertical":
+        Verticle(width, height)
       else:
-          print("\nInvalid pattern choice.\n")
-          Q = input("Press Enter to Continue")
+        print(
+            "Invalid stripe direction. Please enter 'horizontal' or 'vertical'."
+        )
+      break
+    # diagonal code
+    elif pattern == "Diagonal":
+      dir = input("Enter direction (left/right): ")
+      if dir == "left":
+        Diagonal(width, height)
+        break
+      elif dir == "right":
+        DiagonalR(width, height)
+        break
+      else:
+        print("Invalid direction. Please enter 'left' or 'right'.")
+    # dot code
+    elif pattern == "Dot":
+      dot_size = int(input("Size of each Dots: "))
+      spacing = int(input("Spacing between each Dots: "))
+      DotPattern(dot_size, spacing, width, height)
+      break
+    # diagrid code
+    elif pattern == "Diagrid":
+      Diagonal(width, height)
+      DiagonalR(width, height)
+      break
+    else:
+      print("\nInvalid pattern choice.\n")
+      Q = input("Press Enter to Continue")
 
 
 # function 2.1 (Verticle lines)
@@ -200,26 +217,29 @@ def DiagonalR(width, height):
 # function 3 (foreground design options?)
 def draw_shape(screen):
   shape = input(
-    "Enter the shape you want to draw (diamond, square, circle): ").lower()
+      "Enter the shape you want to draw (diamond, square, circle): ").lower()
   # user inputs for shape
   color = str()
   color = input("\nEnter the desired color for shape: ")
   width, height = screen.window_width(), screen.window_height()
   while shape not in ["diamond", "square", "circle"]:
-    print("Invalid shape choice. Please choose from 'diamond', 'square', or 'circle'.")
+    print(
+        "Invalid shape choice. Please choose from 'diamond', 'square', or 'circle'."
+    )
     print("\n1. Diamond\n2. Square\n3. Circle")
     shape = input("Enter the shape you want to draw: ").lower()
 
   # if structure for shape
   if shape == "diamond":
     draw_diamond(width, height, color)
-    
+
   elif shape == "square":
     draw_square(width, height, color)
-    
+
   elif shape == "circle":
     draw_circle(width, height, color)
-    
+
+
 # function 3.1 (diamond)
 def draw_diamond(width, height, color):
   # Declare Variables
@@ -242,6 +262,7 @@ def draw_diamond(width, height, color):
   turtle.end_fill()
   turtle.hideturtle()
 
+
 # function 3.2 (square)
 def draw_square(width, height, color):
   # Declare Variables
@@ -263,6 +284,7 @@ def draw_square(width, height, color):
   turtle.goto(halfW // 2, halfH // 2)
   turtle.end_fill()
   turtle.hideturtle()
+
 
 # function 3.3 (circle)
 def draw_circle(width, height, color):
@@ -297,21 +319,24 @@ def draw_circle(width, height, color):
 
 # function 4 (logo design text?) Code created by Maylad Hanna
 def draw_text(text_letter):
-    font = fontstyle()
-    turtle.speed(0)
-    turtle.penup()
-    # Adjust the position as needed
-    turtle.goto(0, -60)
-    turtle.pendown()
-    turtle.color("white")
-    turtle.write(text_letter, align='center', font=(font, 80, 'normal'))
-    turtle.hideturtle()
+  font = fontstyle()
+  turtle.speed(0)
+  turtle.penup()
+  # Adjust the position as needed
+  turtle.goto(0, -60)
+  turtle.pendown()
+  turtle.color("white")
+  turtle.write(text_letter, align='center', font=(font, 80, 'normal'))
+  turtle.hideturtle()
+
 
 # function 5 (logo design font?) Idea by Silver Dankha, Revised and Corrected by Sm Fardeen Haque
 def fontstyle():
   while True:
+    # Declare Variables
     font = str()
     print("A. Arial\nB. High Tower Text\nC. Imprint MT Shadow")
+    # input for font
     font = input("Choose Font: ").lower()
     # if structure for choosing font
     if font == "a" or font == "arial":
@@ -326,5 +351,3 @@ def fontstyle():
 
 # call main
 main()
-
-
